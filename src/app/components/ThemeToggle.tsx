c34d7b1786ed4bea9f2b562416ff32541d7560a0
cjy <wlsdud3957@naver.com>
2025-06-19 23:27:38 +0900

    다크모드 추가 / 일부 주석

--- src/app/components/ThemeToggle.tsx
+++ src/app/components/ThemeToggle.tsx
@@ -6,10 +6,12 @@ type Props = {
 };
 
 export const ThemeToggle = ({ toggle, isDark }: Props) => {
+  if (typeof window === "undefined") return;
+
   return (
     <button
       onClick={toggle}
-      className="fixed w-12 h-12 text-center bottom-4 right-4 p-2 bg-black dark:bg-white rounded-full z-10"
+      className="fixed w-12 h-12 text-center bottom-8 right-8 p-2 bg-black dark:bg-white rounded-full z-10"
     >
       {isDark ? "☀️" : "🌙"}
     </button>
